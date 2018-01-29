138a6689a0997cacf132c81f4dd86243c536478b
ساره عبدالله <sarhbdallh@MacBook-Pro-alkhas-b-sarh.local>
2018-01-29 11:37:39 +0300

    sarah helwa

--- kjnojno/ViewController.swift
+++ kjnojno/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var sarah = "love"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
